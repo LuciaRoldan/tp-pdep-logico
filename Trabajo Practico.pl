992c16b09f9215c6a243d7e2f4ee3928395fdfa3
Unknown <lucia_roldan@outlook.com>
2017-05-16 22:59:09 -0300

    Correcciones en Propiedades y Usuarios
    Lo de Chameleon no se si esta bien

--- Trabajo Practico.pl
+++ Trabajo Practico.pl
@@ -1,21 +1,33 @@
-%propiedades
-propiedad(tinsmithCircle1774,3,si,0).
-propiedad(avMoreno708,7,si,30).
-propiedad(avSiempreViva742,4,si,0).
-propiedad(calleFalsa123,3,nop,0).
-%usuarios
-usuario(carlos,3,si,_).
-usuario(ana,3,_,100).
-usuario(maria,2,_,15).
-usuario(pedro,2,_,15).
-usuario(chamaleon,_,_,_).
+%%%%%%%%%%%%%%%%%%   PROPIEDADES   %%%%%%%%%%%%%%%%%%
+%(nombre, ambientes, jardín?, metros cubicos de la piscina)
+%Los precios no importan? (700/ 2000/ 1000/ 200)
+propiedad(tinsmithCircle1774, 3, si, 0).
+propiedad(avMoreno708, 7, si, 30).
+propiedad(avSiempreViva742, 4, si, 0).
+propiedad(calleFalsa123, 3, no, 0).
 
-cumpleConCaracteristica(Propiedad,Caracteristica) :-
-  Propiedad(_,Ambientes,Jardin,Piscina),
-  Caracteristica(Ambientes,Jardin,Piscina).
+%%%%%%%%%%%%%%%%%%   USUARIOS   %%%%%%%%%%%%%%%%%%
+usuario(carlos, 3, si, _).
+usuario(ana, _, _, 100).
+usuario(maria, 2, _, 15).
+usuario(pedro, unosAmbientes, unjardin, unosMetrosCubicos) :-
+  usuario(maria, unosAmbientes, unjardin, unosMetrosCubicos).
+%Chamaleon:
+usuario(chamaleon, unosAmbientes, unjardin, unosMetrosCubicos) :-
+  usuario(maria, unosAmbientes, unjardin, unosMetrosCubicos).
+usuario(chamaleon, unosAmbientes, unjardin, unosMetrosCubicos) :-
+  usuario(carlos, unosAmbientes, unjardin, unosMetrosCubicos).
+usuario(chamaleon, unosAmbientes, unjardin, unosMetrosCubicos) :-
+  usuario(ana, unosAmbientes, unjardin, unosMetrosCubicos).
+usuario(chamaleon, unosAmbientes, unjardin, unosMetrosCubicos) :-
+  usuario(pedro, unosAmbientes, unjardin, unosMetrosCubicos).
 
-cumpleConCaracteristica(Propiedad,Caracteristica) :-
-  Propiedad(_,Ambientes,Jardin,Piscina),
-  Caracteristica(Ambientes2,Jardin,Piscina2),
+cumpleConCaracteristica(Propiedad, Caracteristica) :-
+  Propiedad(_, Ambientes, Jardin, Piscina),
+  Caracteristica(Ambientes, Jardin, Piscina).
+
+cumpleConCaracteristica(Propiedad, Caracteristica) :-
+  Propiedad(_, Ambientes, Jardin, Piscina),
+  Caracteristica(Ambientes2, Jardin, Piscina2),
   Ambientes2 >= Ambientes,
   Piscina2 >= Piscina.
