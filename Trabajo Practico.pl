07a339b7e46933633ab894260db788604ab71fde
Ezequiel <ebianchetta@icloud.com>
2017-05-17 12:27:29 -0300

    Funcion compararAmbiente

--- Trabajo Practico.pl
+++ Trabajo Practico.pl
@@ -1,10 +1,12 @@
 %%%%%%%%%%%%%%%%%%   PROPIEDADES   %%%%%%%%%%%%%%%%%%
 %(nombre, ambientes, jardín?, metros cubicos de la piscina)
 %Los precios no importan? (700/ 2000/ 1000/ 200)
-propiedad(tinsmithCircle1774, 3, si, 0).
-propiedad(avMoreno708, 7, si, 30).
-propiedad(avSiempreViva742, 4, si, 0).
-propiedad(calleFalsa123, 3, no, 0).
+
+propiedad(tinsmith_Circle_1774, 3, si, 0).
+propiedad(av_Moreno_708, 7, si, 30).
+propiedad(av_Siempre_Viva_742, 4, si, 0).
+propiedad(calle_Falsa_123, 3, no, 0).
+
 
 %%%%%%%%%%%%%%%%%%   USUARIOS   %%%%%%%%%%%%%%%%%%
 usuario(carlos, 3, si, _).
@@ -22,12 +24,17 @@ usuario(chamaleon, unosAmbientes, unjardin, unosMetrosCubicos) :-
 usuario(chamaleon, unosAmbientes, unjardin, unosMetrosCubicos) :-
   usuario(pedro, unosAmbientes, unjardin, unosMetrosCubicos).
 
-cumpleConCaracteristica(Propiedad, Caracteristica) :-
-  Propiedad(_, Ambientes, Jardin, Piscina),
-  Caracteristica(Ambientes, Jardin, Piscina).
+compararAmbiente(Nombre1, UnosAmbientes, UnJardin, UnosMetrosCubicos) :-
+  propiedad(Nombre1, UnosAmbientes, UnJardin, UnosMetrosCubicos),
+  propiedad(Nombre2, UnosAmbientes, _, _),
+  Nombre1 \= Nombre2.
 
-cumpleConCaracteristica(Propiedad, Caracteristica) :-
-  Propiedad(_, Ambientes, Jardin, Piscina),
-  Caracteristica(Ambientes2, Jardin, Piscina2),
-  Ambientes2 >= Ambientes,
-  Piscina2 >= Piscina.
+%cumpleConCaracteristica(Propiedad, Caracteristica) :-
+%  Propiedad(_, Ambientes, Jardin, Piscina),
+%  Caracteristica(Ambientes, Jardin, Piscina).
+
+%cumpleConCaracteristica(Propiedad, Caracteristica) :-
+%  Propiedad(_, Ambientes, Jardin, Piscina),
+%  Caracteristica(Ambientes2, Jardin, Piscina2),
+%  Ambientes2 >= Ambientes,
+%  Piscina2 >= Piscina.
