07516a0cbf0a15e5d025e924d5b4669fb5b2f3ab
Unknown <lucia_roldan@outlook.com>
2017-05-17 22:06:40 -0300

    Agregue lo de hoy y algo mas
    cumpleConCaracteristica
    cumpleConCaracteristica2
    nuevo Chameleon

--- Trabajo Practico.pl
+++ Trabajo Practico.pl
@@ -9,29 +9,48 @@ propiedad(calle_Falsa_123, 3, no, 0).
 
 
 %%%%%%%%%%%%%%%%%%   USUARIOS   %%%%%%%%%%%%%%%%%%
-usuario(carlos, 3, si, _).
-usuario(ana, _, _, 100).
+usuario(carlos, 3, si, 0).
+usuario(ana, 0, _, 100).
 usuario(maria, 2, _, 15).
-usuario(pedro, unosAmbientes, unjardin, unosMetrosCubicos) :-
-  usuario(maria, unosAmbientes, unjardin, unosMetrosCubicos).
+usuario(pedro, UnosAmbientes, Unjardin, UnosMetrosCubicos) :-
+  usuario(maria, UnosAmbientes, Unjardin, UnosMetrosCubicos).
 %Chamaleon:
-usuario(chamaleon, unosAmbientes, unjardin, unosMetrosCubicos) :-
-  usuario(maria, unosAmbientes, unjardin, unosMetrosCubicos).
-usuario(chamaleon, unosAmbientes, unjardin, unosMetrosCubicos) :-
-  usuario(carlos, unosAmbientes, unjardin, unosMetrosCubicos).
-usuario(chamaleon, unosAmbientes, unjardin, unosMetrosCubicos) :-
-  usuario(ana, unosAmbientes, unjardin, unosMetrosCubicos).
-usuario(chamaleon, unosAmbientes, unjardin, unosMetrosCubicos) :-
-  usuario(pedro, unosAmbientes, unjardin, unosMetrosCubicos).
+usuario(chamaleon, UnosAmbientes, Unjardin, UnosMetrosCubicos) :-
+  usuario(maria, UnosAmbientes, Unjardin, UnosMetrosCubicos).
+usuario(chamaleon, UnosAmbientes, Unjardin, UnosMetrosCubicos) :-
+  usuario(carlos, UnosAmbientes, Unjardin, UnosMetrosCubicos).
+usuario(chamaleon, UnosAmbientes, Unjardin, UnosMetrosCubicos) :-
+  usuario(ana, UnosAmbientes, Unjardin, UnosMetrosCubicos).
+usuario(chamaleon, UnosAmbientes, Unjardin, UnosMetrosCubicos) :-
+  usuario(pedro, UnosAmbientes, Unjardin, UnosMetrosCubicos).
 
-<<<<<<< HEAD
-=======
+%Chameleon2
+usuario(chamaleon, UnosAmbientes, Unjardin, UnosMetrosCubicos) :-
+  usuario(_, UnosAmbientes, Unjardin, UnosMetrosCubicos).
+
+
 compararAmbiente(Nombre1, UnosAmbientes, UnJardin, UnosMetrosCubicos) :-
   propiedad(Nombre1, UnosAmbientes, UnJardin, UnosMetrosCubicos),
   propiedad(Nombre2, UnosAmbientes, _, _),
   Nombre1 \= Nombre2.
 
->>>>>>> master
+
+cumpleConCaracteristica(Nombre, AmbientesBuscados, Jardin, MetrosCubicosBuscados) :-
+  propiedad(Nombre, AmbientesPosta, Jardin, MetrosCubicosPosta),
+  AmbientesPosta >= AmbientesBuscados,
+  MetrosCubicosPosta >= MetrosCubicosBuscados.
+
+
+cumpleConCaracteristica2(NombrePropiedad, NombrePersona) :-
+  usuario(NombrePersona, AmbientesBuscados, Jardin, MetrosCubicosBuscados),
+  propiedad(NombrePropiedad, AmbientesPosta, Jardin, MetrosCubicosPosta),
+  AmbientesPosta >= AmbientesBuscados,
+  MetrosCubicosPosta >= MetrosCubicosBuscados.
+
+cumpleConCaracteristica3(propiedad(NombrePropiedad, AmbientesPosta, Jardin, MetrosCubicosPosta), usuario(NombrePersona, AmbientesBuscados, Jardin, MetrosCubicosBuscados)) :-
+  AmbientesPosta >= AmbientesBuscados,
+  MetrosCubicosPosta >= MetrosCubicosBuscados.
+
 %cumpleConCaracteristica(Propiedad, Caracteristica) :-
 %  Propiedad(_, Ambientes, Jardin, Piscina),
 %  Caracteristica(Ambientes, Jardin, Piscina).
@@ -41,11 +60,3 @@ compararAmbiente(Nombre1, UnosAmbientes, UnJardin, UnosMetrosCubicos) :-
 %  Caracteristica(Ambientes2, Jardin, Piscina2),
 %  Ambientes2 >= Ambientes,
 %  Piscina2 >= Piscina.
-<<<<<<< HEAD
-
-tienenMismasCaracteristicas(Nombre1, UnosAmbientes, UnJardin, UnaPiscina) :-
-  propiedad(Nombre1, UnosAmbientes, UnJardin, UnaPiscina),
-  propiedad(_, UnosAmbientes, UnJardin, UnaPiscina).
-  %Nombre1 \= Nombre2.
-=======
->>>>>>> master
